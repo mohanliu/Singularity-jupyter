874305c636f92767f071d16f28743127cb50d27b
Chen Chen <chenchen.bme@gmail.com>
2018-08-27 14:39:51 -0500

    Create Singularity_Install.sh

---
+++ Singularity_Install.sh
@@ -0,0 +1,20 @@
+## This installation script is dedicated for AWS EC2 instance running on AWS Linux (CentOS)
+# Target version
+VERSION=2.6.0
+
+# Install prerequsite
+sudo yum install squashfs-tools libarchive-devel -y -q
+wget https://github.com/singularityware/singularity/releases/download/$VERSION/singularity-$VERSION.tar.gz
+tar xvf singularity-$VERSION.tar.gz
+cd singularity-$VERSION
+./configure --prefix=/usr/local
+make
+sudo make install
+
+# Export alias
+alias singularity="/usr/local/bin/singularity"
+echo "alias singularity='/usr/local/bin/singularity'" >> ~/.bashrc
+
+# Test version
+singularity --version
+sudo singularity --version
